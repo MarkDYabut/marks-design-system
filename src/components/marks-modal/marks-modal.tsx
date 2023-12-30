580422c30b146f65e80dbd2463bb5890cc03e5e8
mdotyabut <mark.y@live.com>
2023-12-30 13:26:00 -0500

    add onclick handleCancel to close modal

--- src/components/marks-modal/marks-modal.tsx
+++ src/components/marks-modal/marks-modal.tsx
@@ -1,4 +1,4 @@
-import { Component, Host, h , Prop, getAssetPath} from '@stencil/core';
+import { Component, Host, h, Prop, getAssetPath } from '@stencil/core';
 
 @Component({
   tag: 'marks-modal',
@@ -15,15 +15,19 @@ export class MarksModal {
   @Prop() isopen: boolean;
   @Prop() closeIcon = 'x.svg';
 
+  private handleCancel = () => {
+    this.isopen = false;
+  };
+
   render() {
     return (
       <div class={this.isopen ? 'modal-wrapper isopen' : 'modal-wrapper'}>
-        <div class="modal-overlay" />
+        <div class="modal-overlay" onClick={this.handleCancel}/>
         <div class="modal">
           <div class="header">
             <h6>Modal Header</h6>
-            <div class="close">
-              <img src={getAssetPath(`./assets/${this.closeIcon}`)} alt="close icon" />  
+            <div class="close" onClick={this.handleCancel}>
+              <img src={getAssetPath(`./assets/${this.closeIcon}`)} alt="close icon" />
             </div>
           </div>
           <div class="body">
